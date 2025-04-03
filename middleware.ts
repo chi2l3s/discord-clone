3262e428ce9ed4ef87078074363aab8ba4b09ef2
chi2l3s <166920192+chi2l3s@users.noreply.github.com>
2025-04-03 13:29:05 +0700

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,5 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-// Explicitly set the runtime to Node.js to avoid Edge Runtime compatibility issues
-export const runtime = 'nodejs';
-
 export default authMiddleware({
   publicRoutes: ["/api/uploadthing"],
   // Add this option to use Node.js runtime

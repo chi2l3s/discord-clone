d8079a6d7a8530854b590e35b10a5809df82b795
chi2l3s <166920192+chi2l3s@users.noreply.github.com>
2025-04-03 13:25:02 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,8 +63,6 @@ export default function RootLayout({
       <html lang="ru" suppressHydrationWarning>
         <head>
           <link rel="icon" href="/favicon.ico" sizes="any" />
-          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-          <link rel="manifest" href="/site.webmanifest" />
         </head>
         <body
           className={cn(openSans.className, "bg-white dark:bg-[#313338]")}
